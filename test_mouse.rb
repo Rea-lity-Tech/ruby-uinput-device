c078bcf4c2d7d6443e0a41403adabb1440f9dbff
Jeremy Laviole <poqudrof@gmail.com>
2018-06-15 19:38:19 +0200

    Added test for mouse event.

---
+++ test_mouse.rb
@@ -0,0 +1,29 @@
+require 'uinput/device'
+
+device = Uinput::Device.new do
+    self.name = "Our virtual device"
+    self.type = LinuxInput::BUS_VIRTUAL
+    # self.add_key(:KEY_A)
+
+    self.add_event(:EV_REL)
+    self.add_event(:EV_KEY)
+    self.add_event(:EV_SYN)
+
+    self.add_key(:BTN_LEFT)
+    self.add_key(:BTN_RIGHT)
+    self.add_rel_event(:REL_X)
+    self.add_rel_event(:REL_Y)
+
+end
+
+# kedevice.send_event(:EV_KEY, :KEY_A, 1)
+device.send_event(:EV_SYN, :SYN_REPORT)
+
+10.times do 
+  device.send_event(:EV_REL, :REL_X, 10)
+  device.send_event(:EV_REL, :REL_Y, 10)
+  device.send_event(:EV_SYN, :SYN_REPORT)
+  sleep 0.2
+end
+
+device.destroy
